833bf8ce6b30c8a5259efaf09fe0de7d5b6cb866
RohitTheDeveloper <rohitmour777@gmail.com>
2023-11-03 19:19:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// function validateform(){ 
+//     var tel=document.getElementById("phonenum").value;  
+
+//     if(tel.length<10){  
+//         alert("Phone number must be of atleast 10 digits!");  
+//         return false;  
+//     } else if(isNaN(tel)){
+//         alert("Phone number should not include character!");
+//         return false;
+//     }
+//     return true;
+// }  
+
+let buttonTop =  document.querySelector(".top-up-button")
+const refreshFunction=()=>{
+    if (document.documentElement.scrollTop < 50) {
+        buttonTop.style.display="none";
+    } else {
+        buttonTop.style.display="block";
+    }
+  }
+  refreshFunction();
+  buttonTop.addEventListener("click",()=>{
+    document.documentElement.scrollTop=0;
+  })
+
+  document.addEventListener("scroll",()=>{
+    refreshFunction();
+  })
